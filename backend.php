728c5ce6b56bcedefbcc76087fac2fd7d60f3f6e
samuellindsay <samuel.lindsay@mcvts.org>
2017-06-06 13:30:47 -0400

    Setup file
    Setting up code that is actually useful for coding our product.

--- backend.php
+++ backend.php
@@ -1,3 +1,19 @@
 <?php
+$BirthYear;
+$StartAge = 2017 - $BirthYear;
+
+$ChanceOfDeath = array();
+$ChanceOfLife = array();
+$Discount;
+//Years after start year
+$count = 0;
+$WantsAnuity = false;
+
+//TODO: initialize these variables
+
+//TODO: Run calculations
+
+
+
 
 ?>
